b9160daa3030ef10740b4d7b807c7527fa87b85e
Nicole Zetterblad <nicole.zetterblad@skola.taby.se>
2022-05-15 20:24:37 +0200

    Tog bort bilder och fixade lite små ditaljer

--- script.js
+++ script.js
@@ -51,3 +51,12 @@ function AddtoCart(name, description, price) {
 
 //Add some products to our shopping cart via code or you can create a button with onclick event
 displayShoppingCart();
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
